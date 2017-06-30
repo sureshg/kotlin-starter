6ca0176fe578cb4d3f69e29bc116f5e50ad9fea9
Suresh G <sureshg.g@gmail.com>
2017-06-29 17:23:54 -0700

    Plugin change using new DSL

--- buildSrc/src/main/kotlin/plugins/BookPlugin.kt
+++ buildSrc/src/main/kotlin/plugins/BookPlugin.kt
@@ -2,15 +2,44 @@ package plugins
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
+import org.gradle.kotlin.dsl.*
+import term.fg256
 import java.io.File
 
 /**
  * Custom book Plugin
  */
-class BookPlugin : Plugin<Project> {
+open class BookPlugin : Plugin<Project> {
+
     override fun apply(project: Project) {
-        val books = project.container(Book::class.java)
-        project.extensions.add("books", books)
+        project.run {
+            val books = container(Book::class.java) { name ->
+                Book(name, file("src/docs/$name"))
+            }
+            extensions.add("books", books)
+
+            tasks {
+                val hello by creating {
+                    group = "misc"
+                    doLast { println("Hello Book Plugin!") }
+                }
+
+                "allBooks" {
+                    group = "misc"
+                    dependsOn(hello)
+                    doLast {
+                        println("Task $name for project ${project.name}")
+                        books.forEach { println(it.toString().fg256()) }
+                    }
+                }
+
+                "appRun"(Exec::class) {
+                    group = "misc"
+                    setCommandLine("pwd")
+                }
+            }
+        }
     }
 }
 
